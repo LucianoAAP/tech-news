e0ee5cfecdf09004e24d1254b22c366746a53ca3
Luciano AAP <lucianoalmeidaap@gmail.com>
2022-03-10 21:40:09 -0300

    adiciona requisito 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,14 @@
+from tech_news.database import search_news, find_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news_links = []
+    news = search_news({'title': {'$regex': title, '$options': 'i'}})
+    for news_data in news:
+        news_link = (news_data['title'], news_data['url'])
+        news_links.append(news_link)
+    return news_links
 
 
 # Requisito 7
