f41ee83435bd04f8c712f4f1029c74c1dc44ae1b
Luciano AAP <lucianoalmeidaap@gmail.com>
2022-03-10 22:44:15 -0300

    adiciona requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,27 @@
+from operator import itemgetter
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news_links = []
+    news = find_news()
+    formated_news = []
+    for news_data in news:
+        popularity = news_data['shares_count'] + news_data['comments_count']
+        formated_news.append({
+            'title': news_data['title'],
+            'url': news_data['url'],
+            'popularity': popularity})
+
+    sorted_news = sorted(
+        formated_news, key=lambda row: (-row['popularity'],
+                                        row['title']))
+
+    for news_data in sorted_news:
+        news_link = (news_data['title'], news_data['url'])
+        news_links.append(news_link)
+    return news_links[:5]
 
 
 # Requisito 11
